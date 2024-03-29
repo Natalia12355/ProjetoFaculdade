3248c607d1c8011ae84614527b1d6d271f66305f
Natalia12355 <124624843+Natalia12355@users.noreply.github.com>
2024-03-29 12:06:23 -0300

    Delete sistema_Bancario_Teste directory

--- sistema_Bancario_Teste/src/main/java/com/sistema/ContaCorrente.java
+++
@@ -1,7 +0,0 @@
-package com.sistema;
-
-public class ContaCorrente extends Conta {
-    public ContaCorrente(String numeroConta, Pessoa pessoa, double saldo, String senha) {
-        super(numeroConta, pessoa, "Corrente", saldo, senha);
-    }
-}
